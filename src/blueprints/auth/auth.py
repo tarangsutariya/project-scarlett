152b6a67d166d8874d61f3ff855d4a0651cd67c7
tarangsutariya <105854061+tarangsutariya@users.noreply.github.com>
2023-03-28 23:41:30 +0530

    update permissions

--- src/blueprints/auth/auth.py
+++ src/blueprints/auth/auth.py
@@ -20,7 +20,8 @@ github = oauth.register(
     authorize_url='https://github.com/login/oauth/authorize',
     authorize_params=None,
     api_base_url='https://api.github.com/',
-    client_kwargs={'scope': 'read:org,user:email'},
+    #client_kwargs={'scope': 'user,repo,admin,user:email'},
+    client_kwargs={'scope': 'repo:read,read:org,user:email'},
 )
 
 @auth_bp.route("/")
